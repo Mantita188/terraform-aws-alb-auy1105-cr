fc8d40d05399bd6c779e7978bdb409fb0b66c57c
Constanza <tu-conita551@gmail.com>
2026-06-16 01:22:53 -0400

    feat: modulo independiente de ALB completando los 5 repositorios exigidos

---
+++ main.tf
@@ -0,0 +1,46 @@
+resource "aws_lb" "external" {
+  name               = "${var.environment}-alb-main"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_security_group_id]
+  subnets            = var.public_subnets
+
+  tags = {
+    Name        = "${var.environment}-alb"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_target_group" "web" {
+  name     = "${var.environment}-tg-main"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "80"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web" {
+  target_group_arn = aws_lb_target_group.web.arn
+  target_id        = var.instance_id
+  port             = 80
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.external.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "environment" { type = string }
+variable "vpc_id" { type = string }
+variable "alb_security_group_id" { type = string }
+variable "public_subnets" { type = list(string) }
+variable "instance_id" { type = string }
